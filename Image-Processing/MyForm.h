c7e4bd8c9552ad320c34b48c79e8af9f72d24ed6
Tolga Karahan <tkarahan147@gmail.com>
2018-04-14 21:17:05 +0300

    Open strip added

--- Image-Processing/MyForm.h
+++ Image-Processing/MyForm.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <atlstr.h>
+#include <windows.h>
+#include "imge_bmp.h"
 
 namespace ImageProcessing {
 
@@ -34,12 +37,20 @@ namespace ImageProcessing {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  openToolStripMenuItem;
+	private: System::Windows::Forms::ContextMenuStrip^  contextMenuStrip1;
+	private: System::ComponentModel::IContainer^  components;
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+		BYTE* Buffer;
+		BYTE* Raw_Intensity;
+		int Width, Height;
+		long Size, new_size;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,18 +59,84 @@ namespace ImageProcessing {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->openToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->menuStrip1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->openToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(944, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// openToolStripMenuItem
+			// 
+			this->openToolStripMenuItem->Name = L"openToolStripMenuItem";
+			this->openToolStripMenuItem->Size = System::Drawing::Size(48, 20);
+			this->openToolStripMenuItem->Text = L"Open";
+			this->openToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::openToolStripMenuItem_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this->contextMenuStrip1->Name = L"contextMenuStrip1";
+			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(12, 50);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(394, 422);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(944, 518);
+			this->ClientSize = System::Drawing::Size(1024, 512);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
 			this->Text = L"Image Processing";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void openToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		LPCTSTR input;
+		CString str;
+
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		{
+			str = openFileDialog1->FileName;
+			input = (LPCTSTR)str;
+			Buffer = LoadBMP(Width, Height, Size, input);
+
+			pictureBox1->Width = Width;
+			pictureBox1->Height = Height;
+			pictureBox1->ImageLocation = openFileDialog1->FileName;
+
+		}
+		else MessageBox::Show("Couldn't open the file.");
+	}
 	};
 }
