a730feea90bdf3e0c421f39d8565ec733c75744c
Tolga Karahan <tkarahan147@gmail.com>
2018-04-14 22:58:22 +0300

    histogram operations added

--- Image-Processing/MyForm.h
+++ Image-Processing/MyForm.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <atlstr.h>
 #include <windows.h>
+#include <math.h>
+
 #include "imge_bmp.h"
+#include "MyForm2.h"
 
 namespace ImageProcessing {
 
@@ -40,7 +43,7 @@ namespace ImageProcessing {
 	private: System::Windows::Forms::MenuStrip^  menuStrip1;
 	protected:
 	private: System::Windows::Forms::ToolStripMenuItem^  openToolStripMenuItem;
-	private: System::Windows::Forms::ContextMenuStrip^  contextMenuStrip1;
+
 	private: System::ComponentModel::IContainer^  components;
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 
@@ -49,6 +52,11 @@ namespace ImageProcessing {
 		BYTE* Raw_Intensity;
 		int Width, Height;
 		long Size, new_size;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::ToolStripMenuItem^  histogramOperationsToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  histogramToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  histogramEqualizationToolStripMenuItem;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 
 
@@ -59,22 +67,33 @@ namespace ImageProcessing {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->openToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
+			this->histogramOperationsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->histogramToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->histogramEqualizationToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->menuStrip1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// menuStrip1
 			// 
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->openToolStripMenuItem });
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->openToolStripMenuItem,
+					this->histogramOperationsToolStripMenuItem
+			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(944, 24);
+			this->menuStrip1->Size = System::Drawing::Size(1024, 24);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -85,10 +104,29 @@ namespace ImageProcessing {
 			this->openToolStripMenuItem->Text = L"Open";
 			this->openToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::openToolStripMenuItem_Click);
 			// 
-			// contextMenuStrip1
+			// histogramOperationsToolStripMenuItem
 			// 
-			this->contextMenuStrip1->Name = L"contextMenuStrip1";
-			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
+			this->histogramOperationsToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->histogramToolStripMenuItem,
+					this->histogramEqualizationToolStripMenuItem
+			});
+			this->histogramOperationsToolStripMenuItem->Name = L"histogramOperationsToolStripMenuItem";
+			this->histogramOperationsToolStripMenuItem->Size = System::Drawing::Size(136, 20);
+			this->histogramOperationsToolStripMenuItem->Text = L"Histogram Operations";
+			// 
+			// histogramToolStripMenuItem
+			// 
+			this->histogramToolStripMenuItem->Name = L"histogramToolStripMenuItem";
+			this->histogramToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->histogramToolStripMenuItem->Text = L"Histogram";
+			this->histogramToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::histogramToolStripMenuItem_Click);
+			// 
+			// histogramEqualizationToolStripMenuItem
+			// 
+			this->histogramEqualizationToolStripMenuItem->Name = L"histogramEqualizationToolStripMenuItem";
+			this->histogramEqualizationToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->histogramEqualizationToolStripMenuItem->Text = L"Histogram Equalization";
+			this->histogramEqualizationToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::histogramEqualizationToolStripMenuItem_Click);
 			// 
 			// openFileDialog1
 			// 
@@ -96,17 +134,44 @@ namespace ImageProcessing {
 			// 
 			// pictureBox1
 			// 
-			this->pictureBox1->Location = System::Drawing::Point(12, 50);
+			this->pictureBox1->Location = System::Drawing::Point(12, 27);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(394, 422);
+			this->pictureBox1->Size = System::Drawing::Size(480, 473);
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Location = System::Drawing::Point(532, 27);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(480, 473);
+			this->pictureBox2->TabIndex = 2;
+			this->pictureBox2->TabStop = false;
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(532, 27);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(480, 473);
+			this->chart1->TabIndex = 3;
+			this->chart1->Text = L"chart1";
+			this->chart1->Visible = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1024, 512);
+			this->Controls->Add(this->chart1);
+			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
@@ -115,6 +180,8 @@ namespace ImageProcessing {
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -138,5 +205,80 @@ namespace ImageProcessing {
 		}
 		else MessageBox::Show("Couldn't open the file.");
 	}
-	};
+	
+	private: System::Void histogramToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		Raw_Intensity = ConvertBMPToIntensity(Buffer, Width, Height);
+		BYTE histogram[256] = { 0 };
+
+		for (int row = 0; row < Height; row++)
+		{
+			for (int column = 0; column < Width; column++)
+			{
+				histogram[Raw_Intensity[row * Width + column]]++;
+			}
+		}
+
+		this->chart1->Series->Clear();
+		System::Windows::Forms::DataVisualization::Charting::Series^  series1 =
+			(gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+		this->chart1->Series->Add(series1);
+
+		for (int i = 0; i < 256; i++)
+			series1->Points->AddXY(i, histogram[i]);
+		this->chart1->Visible = true;
+	}
+	private: System::Void histogramEqualizationToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+		
+		Raw_Intensity = ConvertBMPToIntensity(Buffer, Width, Height);
+		int histogram[256] = { 0 };
+		int equalizedHistogram[256] = { 0 };
+		int runningSum = 0;
+
+		for (int row = 0; row < Height; row++)
+		{
+			for (int column = 0; column < Width; column++)
+			{
+				histogram[Raw_Intensity[row * Width + column]]++;
+			}
+		}
+
+		for (int i = 0; i < 256; i++)
+		{
+			runningSum += histogram[i];
+			int index = round(((runningSum / (Width * Height)) * 255));
+			equalizedHistogram[index]++;
+		}
+
+		MyForm2^ histogramForm = gcnew MyForm2;
+		histogramForm->equalizeHistogram(histogram, equalizedHistogram);
+		histogramForm->Show();
+
+		for (int row = 0; row < Height; row++)
+		{
+			for (int column = 0; column < Width; column++)
+			{
+				Raw_Intensity[row * Width + column] = equalizedHistogram[Raw_Intensity[row * Width + column]];
+			}
+		}
+
+		this->chart1->Visible = false;
+
+		pictureBox2->Width = Width;
+		pictureBox2->Height = Height;
+
+		unsigned int row, column;
+		Bitmap^ surface = gcnew Bitmap(pictureBox2->Width, pictureBox2->Height);
+		pictureBox2->Image = surface;
+		Color c;
+		for (row = 0; row < Height; row++)
+		{
+			for (column = 0; column < Width; column++)
+			{
+				c = Color::FromArgb(Raw_Intensity[row*Width + column], Raw_Intensity[row*Width + column], Raw_Intensity[row*Width + column]);
+				surface->SetPixel(column, row, c);
+			}
+		}
+	}
+};
 }
---
+++ Image-Processing/MyForm2.h
@@ -0,0 +1,161 @@
+#pragma once
+#include <Windows.h>
+
+namespace ImageProcessing {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm2
+	/// </summary>
+	public ref class MyForm2 : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm2(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm2()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
+	protected:
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(1, 24);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(385, 300);
+			this->chart1->TabIndex = 0;
+			this->chart1->Text = L"chart1";
+			// 
+			// chart2
+			// 
+			chartArea2->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea2);
+			legend2->Name = L"Legend1";
+			this->chart2->Legends->Add(legend2);
+			this->chart2->Location = System::Drawing::Point(418, 24);
+			this->chart2->Name = L"chart2";
+			series2->ChartArea = L"ChartArea1";
+			series2->Legend = L"Legend1";
+			series2->Name = L"Series1";
+			this->chart2->Series->Add(series2);
+			this->chart2->Size = System::Drawing::Size(385, 300);
+			this->chart2->TabIndex = 1;
+			this->chart2->Text = L"chart2";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(134, 8);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(92, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Original Histogram";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(567, 8);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(103, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Equalized Histogram";
+			// 
+			// MyForm2
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(819, 328);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->chart2);
+			this->Controls->Add(this->chart1);
+			this->Name = L"MyForm2";
+			this->Text = L"MyForm2";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+
+	public: void equalizeHistogram(int* histogram, int* equalizedHistogram)
+	{
+		this->chart1->Series->Clear();
+		System::Windows::Forms::DataVisualization::Charting::Series^  series1 =
+			(gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+		this->chart1->Series->Add(series1);
+
+		for (int i = 0; i < 256; i++)
+			series1->Points->AddXY(i, histogram[i]);
+		this->chart1->Visible = true;
+
+		this->chart2->Series->Clear();
+		System::Windows::Forms::DataVisualization::Charting::Series^  series2 =
+			(gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+		this->chart2->Series->Add(series2);
+
+		for (int i = 0; i < 256; i++)
+			series2->Points->AddXY(i, equalizedHistogram[i]);
+		this->chart2->Visible = true;
+	}
+#pragma endregion
+	};
+}
